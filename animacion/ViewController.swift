6c8c945c38966a5e0b6db09dd2a5d8bef98c909d
Macabuku <macabuku@macbook-pro-de-macabuku.local>
2018-12-22 17:19:54 +0100

    Initial Commit

---
+++ animacion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animacion
+//
+//  Created by Macabuku on 22/12/18.
+//  Copyright © 2018 Macabuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
